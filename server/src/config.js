7112e32bc7e5fe9b2b5e048c369869b5c70d51c4
moferrin <omarferrin2016@gmail.com>
2024-07-31 17:54:31 -0500

    remove back

--- server/src/config.js
+++
@@ -1,1 +0,0 @@
-export const TOKEN_SECRET_KEY = "some secret key";
--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { connectDB } from './db.js';
-
-connectDB();
-const PORT = 4000
-app.listen(PORT);
-
-console.log('server listening on port', PORT);
