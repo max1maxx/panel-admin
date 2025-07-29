e9da45db67d795178e5665804beef94283f5fad7
Jonathan Velasquez <55895230+max1maxx@users.noreply.github.com>
2025-07-29 12:10:13 -0500

    Update axios.acs.js

--- src/api/axios.acs.js
+++ src/api/axios.acs.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://45.173.112.9:3005/api/config'
+    baseURL: 'http://localhost:8080/api/config'
 })
 
 export default instance
